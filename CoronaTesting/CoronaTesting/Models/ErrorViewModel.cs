49c095de6f4217931756ddfb91127e067c876453
FeckMelzer <lukas.f.melzer@gmail.com>
2021-01-26 17:41:40 +0100

    initial commit

---
+++ CoronaTesting/CoronaTesting/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoronaTesting.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
